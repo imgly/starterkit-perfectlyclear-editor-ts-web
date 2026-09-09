0637466317ef041aea0e39204834e989b3061363
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-09-09 20:05:45 +0200

    Update starterkit for v1.82.0-rc.2

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import CreativeEditorSDK from '@cesdk/cesdk-js';
 import { initPerfectlyClearEditor } from './imgly';
 import { resolveAssetPath } from './imgly/resolveAssetPath';
 
+
 // ============================================================================
 // Configuration
 // ============================================================================
